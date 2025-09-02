52bf8e2cfd001a15388c2719d03fb7b6e90cbf8d
yuzu-yunoma <yunoma.dev@gmail.com>
2025-09-02 23:55:44 +0900

    Update holiday.js

--- kadai_010/holiday.js
+++ kadai_010/holiday.js
@@ -1,7 +1,7 @@
 const holidays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"]
 
 // for文の場合
-for (let i = 0; i <= holidays.length; i++) {
+for (let i = 0; i < holidays.length; i++) {
   console.log(holidays[i]);
 }
 
@@ -10,4 +10,5 @@ let i = 0;
 while (i < holidays.length) {
   console.log(holidays[i]);
   i++;
+
 }
